e4e417dd7fdde95dac3482a818606486417648b9
Parker Mortensen <parker.b.mortensen@gmail.com>
2024-11-28 08:26:45 -0700

    fixed undefined user bug in requireRegistration middleware file.

--- middleware/requireRegistration.js
+++ middleware/requireRegistration.js
@@ -1,5 +1,5 @@
 function requireRegistration(req, res, next) {
-    if (req.user.isRegistrationComplete) {
+    if (req.user && req.user.isRegistrationComplete) {
         next(); // Allow access
     } else {
         res.redirect('/register');
